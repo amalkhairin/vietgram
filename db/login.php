fe6af20281e865edce7ccdb2de204f52aff12e2f
amalkhairin <amallanch45@gmail.com>
2020-03-10 10:07:56 +0700

    login

---
+++ db/login.php
@@ -0,0 +1,20 @@
+<?php
+    include_once('conn.php');
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $query = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($query);
+
+    if($result->num_rows > 0){
+        session_start();
+        $_SESSION["username"] = $username;
+        
+        header('location: ../feed.php');
+    } else {
+        header('location: ../index.php');
+    }
+
+    $conn->close();
+?>
